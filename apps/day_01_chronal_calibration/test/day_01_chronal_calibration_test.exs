bbd60ab20891523b28eef6684a351ba912709d75
Fred Wu <ifredwu@gmail.com>
2018-12-03 00:08:02 +1100

    Use `async: true` in tests

--- apps/day_01_chronal_calibration/test/day_01_chronal_calibration_test.exs
+++ apps/day_01_chronal_calibration/test/day_01_chronal_calibration_test.exs
@@ -1,5 +1,5 @@
 defmodule Day01ChronalCalibrationTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   doctest ChronalCalibration
   doctest ChronalCalibration.Output
--- apps/day_02_inventory_management_system/test/day_02_inventory_management_system_test.exs
+++ apps/day_02_inventory_management_system/test/day_02_inventory_management_system_test.exs
@@ -1,5 +1,5 @@
 defmodule Day02InventoryManagementSystemTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   doctest InventoryManagementSystem
   doctest InventoryManagementSystem.Utils
